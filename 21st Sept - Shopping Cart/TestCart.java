7a5cd1d39780a6451cc5f44fa2ef404a024cad10
Snehasish <100360925+Snehasish1099@users.noreply.github.com>
2022-09-22 14:23:32 +0530

    Update TestCart.java

--- 21st Sept - Shopping Cart/TestCart.java
+++ 21st Sept - Shopping Cart/TestCart.java
@@ -4,18 +4,19 @@ Program: Creation and updation of the Cart
 @date: 21st Sept, 2022
 */
 
-
+//Creating class TestCart
 class TestCart
 {
+	//Declaring the main method
 	public static void main(String args[])
 	{	
 		//Constructor injection
 		Item cake = new Item("Pure Chocolate", 10, 40);
-		Item chips = new Item("Kurkure-Hyderabadi Hungama", 10, 20 );
+		Item chips = new Item("Kurkure-Hyderabadi Hungama", 10, 20 );        //Passing values inside the arguements
 		Item chocolate = new Item("Amul's Dark Chocolate", 5, 100);
 		Item drinks = new Item("2L Cocacola", 5, 85);
 		
-		ShoppingCart myCart = new ShoppingCart();
+		ShoppingCart myCart = new ShoppingCart();       //Creating new object myCart
 		
 		//Updating the cart
 		myCart.addItem(cake);
